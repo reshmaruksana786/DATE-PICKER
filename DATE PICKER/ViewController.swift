9699521acb2d2c4230bc6991793c4070c315af06
Syed.Reshma Ruksana <reshmasmile468@achiever-8.local>
2019-10-24 15:13:14 +0530

    Initial Commit

---
+++ DATE PICKER/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DATE PICKER
+//
+//  Created by Syed.Reshma Ruksana on 24/10/19.
+//  Copyright © 2019 Syed.Reshma Ruksana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
